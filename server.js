2e2d7e38264964bdabed59ec784824f5ca336d85
Laura Aupert <aupert.laura@gmail.com>
2021-03-15 22:39:35 -0400

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,16 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 require("./routes/api")(app);
 require('./routes/html-routes')(app)
